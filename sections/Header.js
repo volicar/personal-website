6573d80ae229b75fadf0c497be04065affa71e33
Vinicius Oliveira <vinicius.oliveira@ed3.com.br>
2022-06-13 02:16:48 -0300

    design done

--- sections/Header.js
+++ sections/Header.js
@@ -1,6 +1,25 @@
+import Link from "next/link";
+
 const Header = () => {
+
+  const navigations = [
+    { label: 'Home', path:'/'},
+    { label: 'About', path:'/about'},
+  ];
+
   return (
-    <div>Header</div>
+    <header className="h-16 flex items-center justify-between">
+      <ul className="flex gap-4">
+        {navigations.map(nav => (
+          <Link href={nav.path}><a 
+            className="font-semibold text-gray-400 hover:text-gray-500">
+            
+            {nav.label}</a></Link>
+          
+        ))}
+      </ul>
+      <button>Icon</button>
+    </header>
   )
 }
 
