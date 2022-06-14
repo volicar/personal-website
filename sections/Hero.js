2857aec9226e7f9d2ebb016091a66c2e3618b318
Vinicius Oliveira <vinicius.oliveira@ed3.com.br>
2022-06-14 02:06:28 -0300

    add style and button

--- sections/Hero.js
+++ sections/Hero.js
@@ -1,6 +1,14 @@
+import Button from '../components/Button';
+
 const Hero = () => {
   return (
-    <div>Hero</div>
+    <section className="mt-16">
+      <h1 className="text-7xl font-bold"> Hi I'm Vinícius </h1>
+      <h3 className="text-4xl my-3">I am Software Engineer</h3>
+      <p className="text-gray-700 mb-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore eaque velit sunt ut fugit maiores tempore enim tempora. Beatae laboriosam recusandae exercitationem pariatur quo velit,
+         molestiae voluptatibus mollitia labore totam.</p>
+         <Button>Hire Me!</Button>
+    </section>
   )
 }
 
