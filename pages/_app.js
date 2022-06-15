176153f63707b405b37b2943a78c2946749d57db
Vinicius Oliveira <vinicius.oliveira@ed3.com.br>
2022-06-15 03:48:14 -0300

    darkmode on

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,14 @@
+import { ThemeProvider } from 'next-themes'
 import Layout from '../components/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <ThemeProvider>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </ThemeProvider>
   )
 }
 
