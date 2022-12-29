d7530e8e83bf6e1b0c1b1d78d4f9177a5daa45a0
Vinicius Oliveira <vinicius.oliveira@ed3.com.br>
2022-12-29 20:46:54 -0300

    fit: content page about and project

--- pages/404.js
+++ pages/404.js
@@ -1,7 +1,7 @@
 
 const NotFound = () => {
   return (
-    <div className="text-center text-gray-600 font-semibold mt-20 text-2xl">Ops! Page not found 😢</div>
+    <div className="text-center text-gray-600 font-semibold mt-20 text-5xl">Ops! Page not found 😢</div>
   )
 }
 
--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 
 const About = () => {
   return (
-    <div className="text-center text-gray-600 font-semibold mt-20 text-2xl">About</div>
+    <div className="text-center text-gray-600 font-semibold mt-20 text-5xl">Content coming soon... 🌱</div>
   )
 }
 
